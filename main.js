0e001cc1afefd3b7af859c3eb0c9cb289c78f023
0DiegoM <98572984+0DiegoM@users.noreply.github.com>
2022-05-31 19:13:30 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 var lista_de_invitados_1 = []
 
-function submit() {
+
+function sumbit_send() {
     var guestname = document.getElementById('lista_de_invitados_ingresar').Value;
     lista_de_invitados_1.push(guestname);
-
+    var length_of_name = lista_de_invitados_1.length;
+document.getElementById('lista_de_invitados_ingresar_1').innerHTML = lista_de_invitados_1.toString();
     document.getElementById('lista_de_invitados_ingresar').style.display = 'inline-block'
+}
+function Show() {
+var i = lista_de_invitados_1.join('<br>');
+document.getElementById('p1').innerHTML = i.toString();
+document.getElementById('Sort_button').style.display = 'block';
+}
+
+function Sort() {
+lista_de_invitados_1.sort();
+var i = lista_de_invitados_1.join('<br>');
+document.getElementById('Sort_list').innerHTML = i.toString();
 }
 
 function searching() {
-var a = document.getElementById('').Value;
-let found = 0;
+var a = document.getElementById('searched').Value;
+var found = 0;
 var b;
 
 for (b = 0; b<lista_de_invitados_1.length; b++)
@@ -18,6 +31,9 @@ for (b = 0; b<lista_de_invitados_1.length; b++)
 found=found+1;
     }
 }
-document.getElementById('').innerHTML = 'name found'+ found +'time/b';
+document.getElementById('searched').innerHTML = 'name found'+ found +'time/b';
 console.log('found name'+ found +' time/b');
 }
+
+
+
